c28e295633d6221176114d27493f1e781c97504f
Sem van der Schilt <302862459@student.rocmondriaan.nl>
2018-03-14 12:01:13 +0100

    add part 5 of security to the project

---
+++ Symfony/src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository {
+    
+}
