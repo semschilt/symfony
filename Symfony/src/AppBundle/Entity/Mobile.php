f60cd817f6d13d910f72288c05a879dbd52e00ac
Sem van der Schilt <302862459@student.rocmondriaan.nl>
2018-03-14 12:35:34 +0100

    add symfony and databases to the project

---
+++ Symfony/src/AppBundle/Entity/Mobile.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
+
+/**
+ * @ORM\Table(name="mobile")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MobileRepository");
+ */
+class Mobile {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length= 64)
+     */
+    private $naam;
+    /**
+     * @ORM\Column(type="string", length= 64)
+     */
+    private $prijs;
+    /**
+     * @ORM\Column(type="string", length= 64)
+     */
+    private $type;
+
+    public function serialize() {
+        return serialize(array(
+            $this->id,
+            $this->naam,
+            $this->prijs,
+            $this->type,
+        ));
+    }
+
+    public function unserialize($serialized) {
+        list (
+            $this->id,
+            $this->naam,
+            $this->prijs,
+            $this->type,
+        ) = unserialize($serialized);
+    }
+
+    public function getNaam(){
+        return $this->naam;
+    }
+
+    public function setNaam() {
+        $this->naam = $naam;
+    }
+
+    public function getPrijs(){
+        return $this->prijs;
+    }
+
+    public function setPrijs() {
+        $this->prijs = $prijs;
+    }
+
+    public function getType(){
+        return $this->type;
+    }
+
+    public function setType() {
+        $this->type = $type;
+    }
+}
